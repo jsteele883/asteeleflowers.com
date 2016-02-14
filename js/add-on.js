a23c662bfbac4a4a28ef7e8149c15b6fc24ccecc
jsteele883 <jsteele883@gmail.com>
2016-02-14 18:12:28 +0000

    Layout Changes
    Overall changes to layout and changes to the contact page.
    Known issues:
    -form submission doesn’t work yet
    -fav icon required
    -potentially background change

---
+++ js/add-on.js
@@ -0,0 +1,11 @@
+
+function toggleChevron(e) {
+	$(e.target)
+	.prev('.panel-heading')
+	.find("i.indicator")
+	.toggleClass('glyphicon-minus-sign glyphicon-plus-sign');
+}
+$('#accordion').on('hidden.bs.collapse', toggleChevron);
+$('#accordion').on('shown.bs.collapse', toggleChevron);
+
+$('.carousel').carousel();
